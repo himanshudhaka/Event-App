8bf69f24a87e890c425a69b9b8d57197bc8eb06a
Himanshu Dhaka <himanshudhaka1701@gmail.com>
2023-08-26 12:40:54 +0530

    new changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCLP6P3h8wJcaGU4595vrAs_X7X29mxGaw',
+    authDomain: 'shows-520f1.firebaseapp.com',
+    projectId: 'shows-520f1',
+    storageBucket: 'shows-520f1.appspot.com',
+    messagingSenderId: '1022058996784',
+    appId: '1:1022058996784:web:47f56679f372d42281f9f5',
+    measurementId: 'G-DY8PP3T4W8',
+  },
 };
